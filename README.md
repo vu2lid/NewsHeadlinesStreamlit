0fa425816791c3461e1300e914495cd48b4652dc
vu2lid <vu2lid@gmail.com>
2020-08-04 00:33:45 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
+
 # NewsHeadlinesStreamlit
 News headlines visualization using Python 3, NumPy, pandas, Streamlit and pydeck
+
+A collection of news headlines as JSON data files from a number of publicly availabe news sources (these are from the time period between the end of June 2020 and the end of July 2020). A simple Streamlit app to analyze and display the data.
+
+## Getting Started
+
+The following instructions will get you a copy of the project up and running on your local machine.
+
+### Prerequisites
+
+You will need:
+```
+Python 3, NumPy, pandas, Streamlit and pydeck
+``` 
+### Installing
+
+Setup a Python 3 virtual environment and install the prerequesites.
+
+## Running
+
+Run the app by:
+```
+streamlit run app.py
+```
+
+## Built With
+
+* [Streamlit](https://www.streamlit.io/) - ML app framework 
+* [pandas](https://pandas.pydata.org/) - Data handling 
+* [NumPy](https://numpy.org/) - Mathematical functions 
+* [pydeck](https://pypi.org/project/pydeck/) - Data visualization 
+
+## License
+
+This project is licensed under the GPLv3 License
---
+++ app.py
@@ -0,0 +1,81 @@
+import numpy as np
+import os
+import pandas as pd
+import pydeck
+import streamlit as st
+
+data_root_processed = 'data'
+
+
+@st.cache(persist=False, allow_output_mutation=False)
+# @st.cache(persist=True)
+def load_data(input_json_data):
+    data = pd.read_json(input_json_data)
+    return data
+
+
+st.title("Analyzing RSS News Feeds")
+st.markdown("This is a dashboard for analyzing and visualizing headlines of RSS news feeds "
+            "from some of the popular news providers on the web")
+
+# List of news sources we have
+display_source = ('bbc', 'cnn', 'dailystar', 'guardian', 'ht', 'jpost', 'khaleejtimes',
+                  'npr', 'nyt', 'pd', 'rt', 'thehindu', 'toi', 'wp', 'xinhua', 'yahoonews')
+
+options = list(range(len(display_source)))
+sel = st.selectbox("News source:", options,
+                   format_func=lambda x: display_source[x])
+source = display_source[sel]
+target = os.path.join(data_root_processed, source+'.json')
+data = load_data(target)
+unmodified_data = data
+
+st.header(
+    f"News headlines of {source} between {data['date_time'].min()} and {data['date_time'].max()}")
+
+st.header(
+    f"Which locations in the world are connected to the news headlines from {source} ?")
+st.map(data.query("source == @source")[["latitude", "longitude"]], zoom=0.7)
+
+sentiment_dict = {'Negative': 0, 'Positive': 1}
+sentiment_tup = tuple(sentiment_dict)
+sentiment_options = list(range(len(sentiment_tup)))
+sentiment_sel = st.selectbox("News sentiment:", sentiment_options,
+                             format_func=lambda x: sentiment_tup[x])
+data = unmodified_data[unmodified_data['sentment'] == sentiment_sel]
+st.header(
+    f"From where are the news headlines with {sentiment_tup[sentiment_sel]} sentiment being reported by {source} ?")
+
+# Calculate average latitude and longitude of all the locations so that map can be centered
+map_center = (np.average(data['latitude']), np.average(data['longitude']))
+
+# Define a layer to display on a map
+layer = pydeck.Layer(
+    'HexagonLayer',
+    data,
+    get_position=['longitude', 'latitude'],
+    radius=70000,
+    auto_highlight=True,
+    elevation_scale=50,
+    pickable=True,
+    elevation_range=[0, 3000],
+    extruded=True,
+    coverage=1)
+
+# Set the viewport location
+view_state = pydeck.ViewState(
+    longitude=map_center[1],
+    latitude=map_center[0],
+    zoom=2,
+    min_zoom=1,
+    max_zoom=15,
+    pitch=50,
+    bearing=0.0)
+
+# Streamlit render
+st.write(pydeck.Deck(layers=[layer], initial_view_state=view_state))
+
+# Display unmodified data if checkbox is selected
+if st.checkbox("Show Map Data", False):
+    st.subheader('Map Data')
+    st.write(unmodified_data)
